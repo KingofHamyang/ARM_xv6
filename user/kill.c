814d7608a99a153abb2f562f0523809b3ce64818
Vaelot <minusgodkeanu@gmail.com>
2019-10-11 21:11:05 +0900

    refactor total project structure(WIP)

---
+++ user/kill.c
@@ -0,0 +1,17 @@
+#include "../include/types.h"
+#include "../include/stat.h"
+#include "../include/user.h"
+
+int
+main(int argc, char **argv)
+{
+  int i;
+
+  if(argc < 2){
+    printf(2, "usage: kill pid...\n");
+    exit();
+  }
+  for(i=1; i<argc; i++)
+    kill(atoi(argv[i]));
+  exit();
+}
